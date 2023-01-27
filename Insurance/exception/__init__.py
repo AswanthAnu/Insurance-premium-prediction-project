a2a2b75a803e1eb99ff58608a5ed18ebe92ea5fa
AswanthAnu <aswanthanu777@gmail.com>
2023-01-28 00:34:58 +0530

    data ingestion

--- Insurance/exception/__init__.py
+++ Insurance/exception/__init__.py
@@ -9,23 +9,23 @@ class InsuranceException(Exception):
         self.error_message = InsuranceException.error_message_detail(error_message, error_detail=error_detail)
 
     @staticmethod
-    def error_message_detail(error_message: Exception, error_detail:sys)->str:
+    def error_message_detail(error: Exception, error_detail:sys)->str:
         """
         error: Exception object raise from module
         error_detail: is sys module contains detail information about system execution information.
         """
         _, _, exc_tb = error_detail.exc_info() # getting the traceback object
         
-        line_number = exc_tb.tb_frame.f_lineno # getting the line number where exception occurred
+        line_number = exc_tb.tb_lineno # getting the line number where exception occurred
 
         file_name = exc_tb.tb_frame.f_code.co_filename # getting the file name where exception occurred
 
         # preparing error message
         error_message = f"Error occured python script name [{file_name}]" \
-                        f" line number [{line_number}] error message[{Exception}]."
+                        f" line number [{line_number}] error message[{error}]."
 
 
         return error_message # formatting the error message string with file name, line number, and error message
 
     def __str__(self):
-        return InsuranceException.__name__.__str__() # returning the name of the exception class as a string
+        return self.error_message # returning the exception details
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 import os, sys
 from Insurance.utils import get_collection_as_dataframe
+from Insurance.entity import config_entity, artifact_entity
+from Insurance.components.data_ingestion import DataIngestion
 
 def test_logger_and_exception():
     try:
@@ -16,8 +18,11 @@ def test_logger_and_exception():
 if __name__ == '__main__':
     try:
         # test_logger_and_exception()
-        print('here')
-        get_collection_as_dataframe(database_name="Insurance_DB", collection_name="Insurance_collection")
-
+        # get_collection_as_dataframe(database_name="Insurance_DB", collection_name="Insurance_collection")
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
     except Exception as e:
         print(e)
